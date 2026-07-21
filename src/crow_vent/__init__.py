bc936c7e89da6eed9c087bb2c93b1ef9ea2fa065
Kristoffer Toivanen <kristoffer.toivanen@chasacademy.se>
2026-07-21 23:55:30 +0200

    Pass 5

--- src/crow_vent/__init__.py
+++ src/crow_vent/__init__.py
@@ -1,5 +1,13 @@
 from .analysis import analyse_system, build_relations, infer_system_kind
 from .classification import classify_candidates
+from .lexicon import (
+    ComponentMatch,
+    DuctDimension,
+    DuctStringMatch,
+    LayerMatch,
+    LayerProfileEngine,
+    VentLexicon,
+)
 from .models import VentClassification, VentComponentDefinition
 from .quantity import build_quantity_takeoff, quantity_takeoff_csv
 from .registry import component_registry, normalise_symbol, resolve_component
@@ -8,6 +16,12 @@ from .service import build_vent_model
 __all__ = [
     "VentClassification",
     "VentComponentDefinition",
+    "VentLexicon",
+    "LayerProfileEngine",
+    "DuctDimension",
+    "DuctStringMatch",
+    "ComponentMatch",
+    "LayerMatch",
     "analyse_system",
     "build_relations",
     "infer_system_kind",
