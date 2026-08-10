df524a87a5f75964f48b7619c2be471c09e181f2
Kristoffer Toivanen <kristoffer.toivanen@chasacademy.se>
2026-08-10 11:30:28 +0200

    pass 98 ombesiktning

--- modules/crow-ovk-module/src/crow_ovk_module/plugin.py
+++ modules/crow-ovk-module/src/crow_ovk_module/plugin.py
@@ -21,6 +21,7 @@ from .ovk_field_surface import ovk_field_router
 from .ovk_field_workbench import ovk_field_workbench_router
 from .ovk_intyg_surface import ovk_intyg_router
 from .ovk_legacy_surface import ovk_legacy_router
+from .ovk_reinspection_surface import ovk_reinspection_router
 from .ovk_reporting_surface import ovk_reporting_router
 from .ovk_surface import ovk_router
 from .ovk_time_surface import ovk_time_router
@@ -68,6 +69,7 @@ class CrowOvkModulePlugin:
             ovk_router(),
             ovk_workflow_router(data_root),
             ovk_intyg_router(data_root),
+            ovk_reinspection_router(data_root),
             ovk_field_router(data_root),
             ovk_field_media_router(data_root),
             ovk_field_workbench_router(data_root),
